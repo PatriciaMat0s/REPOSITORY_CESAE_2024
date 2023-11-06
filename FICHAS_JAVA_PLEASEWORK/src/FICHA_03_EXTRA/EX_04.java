32546ce4a0c6da177a66cda28e7e14a66f49b965
PatriciaMat0s <148441087+PatriciaMat0s@users.noreply.github.com>
2023-11-06 01:14:45 +0000

    proj1 terminado

--- FICHAS_JAVA_PLEASEWORK/src/FICHA_03_EXTRA/EX_04.java
+++ FICHAS_JAVA_PLEASEWORK/src/FICHA_03_EXTRA/EX_04.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class EX_04 {
     public static void main(String[] args) {
 
-// EXERCICIO 4.1:
+        // EXERCICIO 4.1:
         for (int row=1 ; row<=4 ; row++) {
             for (int col = 1; col <= 10; col++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        // EXERCICIO 4.2:
+        for (int row=1 ; row<=5 ; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print("*");
+                System.out.println();
+            }
+
+        }
+
 
 
     }
