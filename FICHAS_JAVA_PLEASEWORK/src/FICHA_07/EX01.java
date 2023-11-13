eaf4cd806699a81a5dd70dfb0218de7f9a41e3c1
PatriciaMat0s <148441087+PatriciaMat0s@users.noreply.github.com>
2023-11-13 16:48:36 +0000

    ficha 07 inicio

---
+++ FICHAS_JAVA_PLEASEWORK/src/FICHA_07/EX01.java
@@ -0,0 +1,25 @@
+
+//1. Escreva um programa que leia o conteúdo de um ficheiro de texto e imprima na consola
+
+
+package FICHA_07;
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class EX01 {
+
+public static void main(String[] args) throws FileNotFoundException {
+
+        File ficheiro = new File("FILES/exercicio_01.txt") ;
+
+        Scanner sc = new Scanner(ficheiro);
+
+        String linha;
+
+        while (sc.hasNextLine()){
+                linha = sc.nextLine();
+                System.out.println(linha);
+                }
+        }
+}
